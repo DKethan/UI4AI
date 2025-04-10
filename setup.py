9f62f4281a4085d5a064bc175890800177d47121
Kethan Dosapati <142290804+DKethan@users.noreply.github.com>
2025-04-10 16:28:38 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="UI4AI",
     version="0.1.0",
-    author="Robin",
+    author="Kethan Dosapati",
     description="Streamlit UI for LLM chat apps",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
